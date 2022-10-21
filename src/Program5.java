30dad97992a63619c86a84fc302c3ed5bdfec806
RohithReddy Gouni <grohithreddy9999@gmail.com>
2022-10-21 01:18:23 -0400

    first commit

---
+++ src/Program5.java
@@ -0,0 +1,23 @@
+
+public class Program5 {
+	public static void main(String[] args)
+	{
+		int n=14;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if (i==1 || i==n || (i>1 && i<=n/2 && j<=n/2-i+1) || (i>n/2 && i<n && j<=n/2 && (i-j)>=(n/2)-1))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
